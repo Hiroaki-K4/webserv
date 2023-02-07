06b28a98bd642d60ecd74e57fd03c2989359544b
Hiroaki-K4 <49no19@gmail.com>
2023-02-07 23:36:57 +0900

    Update open_listen_fd function

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -5,5 +5,11 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <cstring>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#define LISTENQ  1024  // Second argument to listen(). Maximum queue length of pending connections for sockfd.
 
 #endif
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 22:12:06 by hkubo             #+#    #+#             */
-/*   Updated: 2023/02/06 23:05:18 by hkubo            ###   ########.fr       */
+/*   Updated: 2023/02/07 23:35:56 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,43 @@
 
 int open_listen_fd(char *port)
 {
-    std::cout << "Port: " << port << std::endl;
-    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (listen_fd < 0) {
-        std::cout << "[main.cpp open_listen_fd][ERROR] socket function failed." << std::endl;
+    int listen_fd, opt_val = 1;
+    char buf[20];
+    struct addrinfo hints, *listp, *p;
+
+    std::memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_socktype = SOCK_STREAM; // Ordered and reliable.
+    hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG; // AI_PASSIVE->server
+    hints.ai_flags = AI_NUMERICSERV; // port should be string include port number.
+    getaddrinfo(NULL, port, &hints, &listp);
+
+    for (p = listp; p; p = p->ai_next) {
+        if ((listen_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0)
+            continue;
+        setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, (const void *)&opt_val, sizeof(int));
+
+        // bind function ask the kernel to relate server socket address and socket descriptor(listen_fd).
+        if (bind(listen_fd, p->ai_addr, p->ai_addrlen) == 0)
+        {
+            getnameinfo(p->ai_addr, p->ai_addrlen, buf, 20, NULL, 0, NI_NUMERICHOST);
+            std::cout << "Host address: " << buf << std::endl;
+            break;
+        }
+        close(listen_fd);
+    }
+
+    freeaddrinfo(listp);
+    if (!p) {
+        std::cout << "[main.cpp open_listen_fd][ERROR] freeaddrinfo error" << std::endl;
+        return -1;
+    }
+    // Change listen_Fd from active socket to listen socket(wait connection request from client).
+    if (listen(listen_fd, LISTENQ) < 0) {
+        std::cout << "[main.cpp open_listen_fd][ERROR] listen error" << std::endl;
+        close(listen_fd);
         return -1;
     }
 
-    // The option is set so that connection requests can be accepted immediately after rebooting.
-    int opt_val = 1;
-    if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, (const void *)&opt_val, sizeof(int)) == -1) {
-        return -1;
-        std::cout << "[main.cpp open_listen_fd][ERROR] setsockopt function failed." << std::endl;
-    }
-
-    struct sockaddr_in addr;
-    std::memset(&addr, 0, sizeof(addr));
-    *addr.sin_family = AF_INET;
-    // myname.sin_addr.s_addr = inet_addr("129.5.24.1");
-    // /* specific interface */
-    // myname.sin_port = htons(1024);
     return listen_fd;
 }
 
@@ -47,5 +64,8 @@ int main(int argc, char *argv[])
     }
     listen_fd = open_listen_fd(argv[1]);
     std::cout << "listen_fd: " << listen_fd << std::endl;
+    if (listen_fd < 0) {
+        return -1;
+    }
     return 0;
 }
