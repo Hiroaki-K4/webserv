dbc64a453e1cde7bb85dbec9ceab691b374989d2
Hiroaki-K4 <49no19@gmail.com>
2023-02-14 09:33:12 +0900

    Add ci

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+COLOR_RESET="\033[m"
+COLOR_RED="\033[31m"
+COLOR_GREEN="\033[32m"
+
+check_result() {
+	if [ $? -ne 0 ]; then
+		printf "${COLOR_RED}%s:%s %s${COLOR_RESET}\n" "$1" "$2" ' [ERROR]'
+		exit 1
+	fi
+	printf "${COLOR_GREEN}%s:%s %s${COLOR_RESET}\n" "$1" "$2" ' [OK]'
+}
+
+# Test make command
+make > /dev/null
+check_result "make"
+make clean > /dev/null
+check_result "make clean"
+make re > /dev/null
+check_result "make re"
+make fclean > /dev/null
+check_result "make fclean"
+make test > /dev/null
+check_result "make test"
