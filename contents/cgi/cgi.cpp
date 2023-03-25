c8cd1a2ae6ebaf755d5991904d7acbbb9bbc7870
Hiroaki-K4 <49no19@gmail.com>
2023-03-25 22:17:14 +0900

    Update cgi program

---
+++ contents/cgi/cgi.cpp
@@ -0,0 +1,28 @@
+#include "cgi.hpp"
+
+int main(int argc, char *argv[])
+{
+    char *buf;
+    char arg[MAXLINE], content[MAXLINE];
+
+    // Extract the two arguments
+    if ((buf = getenv("QUERY_STRING")) != NULL) {
+        strcpy(arg, buf);
+    }
+
+    // Make the response body
+    sprintf(content, "QUERY_STRING=%s", buf);
+    sprintf(content, "<h2>Welcome to Hogwarts!!</h2>");
+    sprintf(content, "%s<h2>Do you want to join %s?.\r\n</h2>", content, buf);
+    sprintf(content, "%s<h1>But you are Satsuma-haaaaaaaaaaan!!\r\n</h1>", content);
+    sprintf(content, "%sGood luck!\r\n", content);
+
+    // Generate the HTTP response
+    printf("Connection close\r\n");
+    printf("Content-length: %d\r\n", (int)strlen(content));
+    printf("Content-type: text/html\r\n\r\n");
+    printf("%s", content);
+    fflush(stdout);
+
+    return 0;
+}
---
+++ contents/cgi/cgi.hpp
@@ -0,0 +1,10 @@
+#ifndef CGI_HPP
+#define CGI_HPP
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#define MAXLINE 8192
+
+#endif
