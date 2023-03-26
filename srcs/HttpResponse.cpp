f1533973629803cee650c645b67a4d39a757fd16
Hiroaki-K4 <49no19@gmail.com>
2023-03-26 15:20:22 +0900

    Add error page

--- srcs/HttpResponse.cpp
+++ srcs/HttpResponse.cpp
@@ -6,21 +6,25 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 14:03:34 by hkubo             #+#    #+#             */
-/*   Updated: 2023/03/26 14:31:06 by hkubo            ###   ########.fr       */
+/*   Updated: 2023/03/26 15:15:33 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HttpResponse.hpp"
 
-HttpResponse::HttpResponse() {}
+HttpResponse::HttpResponse() : http_status(200) {}
 
-HttpResponse::HttpResponse(int conn_fd) { set_conn_fd(conn_fd); }
+HttpResponse::HttpResponse(int conn_fd) : http_status(200) { set_conn_fd(conn_fd); }
 
 HttpResponse::~HttpResponse() {}
 
 void HttpResponse::set_conn_fd(int conn_fd) { this->conn_fd = conn_fd; }
 
 int HttpResponse::get_conn_fd() { return this->conn_fd; }
+
+void HttpResponse::set_http_status(unsigned int http_status) { this->http_status = http_status; }
+
+unsigned int HttpResponse::get_http_status() { return this->http_status; };
 
 int HttpResponse::parse_uri(char *uri, char *filename, char *cgiargs) {
     char *ptr;
@@ -59,7 +63,6 @@ void HttpResponse::get_filetype(char *filename, char *filetype) {
 }
 
 void HttpResponse::serve_static(char *filename, int filesize) {
-    int src_fd;
     char *srcp, filetype[MAXLINE];
 
     // Send response headers to client
@@ -79,9 +82,10 @@ void HttpResponse::serve_static(char *filename, int filesize) {
     std::cout << resp_head;
 
     // Send response body to client
-    src_fd = open(filename, O_RDONLY, 0);
+    int src_fd = open(filename, O_RDONLY, 0);
     if (src_fd == -1) {
         std::cout << "[ERROR] serve_static: File open failed." << std::endl;
+        return;
     }
     srcp = static_cast<char *>(mmap(0, filesize, PROT_READ, MAP_PRIVATE, src_fd, 0));
     close(src_fd);
@@ -104,6 +108,43 @@ void HttpResponse::serve_dynamic(char *filename, char *cgiargs) {
         execve(filename, emptylist, environ);
     }
     wait(NULL);
+}
+
+void HttpResponse::serve_error_page() {
+    char file_name[MAXLINE] = "contents/error.html";
+    struct stat sbuf;
+    if (stat(file_name, &sbuf) < 0) {
+        std::cout << "Cloudn't find this file" << std::endl;
+        return;
+    }
+
+    char filetype[MAXLINE];
+    get_filetype(file_name, filetype);
+    std::stringstream ss;
+    ss << "HTTP/1.0, 200 OK\r\nServer: Tiny Web Server\r\nConnection: close\r\nContent-length: " << sbuf.st_size << "\r\n"
+       << "Content-type: " << filetype << "\r\n\r\n";
+    std::string out;
+    out = ss.str();
+    char resp_head[out.length() + 1];
+    strcpy(resp_head, out.c_str());
+    if (rio_writen(get_conn_fd(), resp_head, strlen(resp_head)) == -1) {
+        std::cout << "rio_writen error!" << std::endl;
+        return;
+    }
+    std::cout << "Response headers:" << std::endl;
+    std::cout << resp_head;
+
+    int src_fd = open(file_name, O_RDONLY, 0);
+    if (src_fd == -1) {
+        std::cout << "[ERROR] serve_static: File open failed." << std::endl;
+        return;
+    }
+    char *srcp;
+    srcp = static_cast<char *>(mmap(0, sbuf.st_size, PROT_READ, MAP_PRIVATE, src_fd, 0));
+    std::cout << "srcp: " << srcp << std::endl;
+    close(src_fd);
+    rio_writen(get_conn_fd(), srcp, sbuf.st_size);
+    munmap(srcp, sbuf.st_size);
 }
 
 void HttpResponse::serve_contents() {
@@ -135,20 +176,29 @@ void HttpResponse::serve_contents() {
     std::cout << "is_static: " << is_static << " filename: " << filename << " cgiargs: " << cgiargs << std::endl;
     struct stat sbuf;
     if (stat(filename, &sbuf) < 0) {
+        set_http_status(404);
         std::cout << "404 Not found: Tiny cloudn't find this file" << std::endl;
-        return;
+        // return;
+        serve_error_page();
     }
     if (is_static) {
         if (!(S_ISREG(sbuf.st_mode) || !(S_IRUSR & sbuf.st_mode))) {  // S_ISREG -> normal file?, S_IRUSR -> have read permission?
+            set_http_status(403);
             std::cout << "403 Forbidden: Tiny couldn't read the file" << std::endl;
-            return;
+            // return;
         }
         serve_static(filename, sbuf.st_size);
     } else {
         if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) {
+            set_http_status(403);
             std::cout << "403 Forbidden: Tiny couldn't run the CGI program" << std::endl;
             return;
         }
         serve_dynamic(filename, cgiargs);
+    }
+
+    if (get_http_status() >= 400) {
+        std::cout << "Bad status: " << get_http_status() << std::endl;
+        serve_error_page();
     }
 }
--- srcs/HttpResponse.hpp
+++ srcs/HttpResponse.hpp
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 14:03:45 by hkubo             #+#    #+#             */
-/*   Updated: 2023/03/26 14:30:21 by hkubo            ###   ########.fr       */
+/*   Updated: 2023/03/26 14:56:37 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,20 @@ class HttpResponse {
 
     void set_conn_fd(int conn_fd);
     int get_conn_fd();
+    void set_http_status(unsigned int http_status);
+    unsigned int get_http_status();
 
     int parse_uri(char *uri, char *filename, char *cgiargs);
     void get_filetype(char *filename, char *filetype);
     void serve_static(char *filename, int filesize);
     void serve_dynamic(char *filename, char *cgiargs);
+    void serve_error_page();
     void serve_contents();
 
 
    private:
     int conn_fd;
+    unsigned int http_status;
 };
 
 #endif
