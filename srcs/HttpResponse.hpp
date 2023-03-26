db92059b4b18e6a389bba14aa7cc33edebc9e6ec
Hiroaki-K4 <49no19@gmail.com>
2023-03-26 17:48:18 +0900

    Refactor HttpResponse class

--- srcs/HttpResponse.hpp
+++ srcs/HttpResponse.hpp
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 14:03:45 by hkubo             #+#    #+#             */
-/*   Updated: 2023/03/26 14:56:37 by hkubo            ###   ########.fr       */
+/*   Updated: 2023/03/26 17:28:03 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,17 +26,33 @@ class HttpResponse {
     int get_conn_fd();
     void set_http_status(unsigned int http_status);
     unsigned int get_http_status();
+    void set_is_static(bool is_static);
+    bool get_is_static();
+    void set_file_name(char *file_name);
+    char *get_file_name();
+    void set_cgi_args(char *cgi_args);
+    char *get_cgi_args();
+    void set_file_info(struct stat file_info);
+    struct stat get_file_info();
 
-    int parse_uri(char *uri, char *filename, char *cgiargs);
-    void get_filetype(char *filename, char *filetype);
-    void serve_static(char *filename, int filesize);
-    void serve_dynamic(char *filename, char *cgiargs);
+    bool parse_uri(char *uri, char *file_name, char *cgi_args);
+    void get_filetype(char *file_name, char *filetype);
+    void serve_static(char *file_name, int filesize);
+    void serve_dynamic(char *file_name, char *cgi_args);
     void serve_error_page();
+    void check_http_request();
     void serve_contents();
+
 
    private:
     int conn_fd;
+    bool is_static;
     unsigned int http_status;
+
+    // request info
+    char file_name[MAXLINE];
+    char cgi_args[MAXLINE];
+    struct stat file_info;
 };
 
 #endif
