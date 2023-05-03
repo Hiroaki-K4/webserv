2d8b433d1d1dabebde1b4a796ba1c0dd86516223
Hiroaki-K4 <49no19@gmail.com>
2023-05-03 17:49:07 +0900

    Support multiple servers

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 22:12:06 by hkubo             #+#    #+#             */
-/*   Updated: 2023/05/03 17:21:10 by hkubo            ###   ########.fr       */
+/*   Updated: 2023/05/03 17:48:39 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,24 +27,28 @@ int main(int argc, char *argv[]) {
     }
 
     std::vector<ServerConfig *> servers = config_parser.get_servers();
-    std::stringstream ss;
-    ss << servers[0]->get_port();
-    char host_name[MAXLINE], port[MAXLINE];
-    strcpy(host_name, servers[0]->get_host_name().c_str());
-    strcpy(port, ss.str().c_str());
-    int listen_fd;
-    if (servers[0]->get_host_name() == "") {
-        listen_fd = open_listen_fd(NULL, port);
-    } else {
-        listen_fd = open_listen_fd(host_name, port);
-    }
-    std::cout << "[INFO] main: listen fd is " << listen_fd << std::endl;
-    if (listen_fd < 0) {
-        return EXIT_FAILURE;
-    }
 
-    HttpServer server(listen_fd);
-    server.multiple_io_server_run(*(servers[0]));
+    // TODO: Support multiple servers by using thread
+    for (unsigned int i = 0; i < servers.size(); i++) {
+        std::stringstream ss;
+        ss << servers[i]->get_port();
+        char host_name[MAXLINE], port[MAXLINE];
+        strcpy(host_name, servers[i]->get_host_name().c_str());
+        strcpy(port, ss.str().c_str());
+        int listen_fd;
+        if (servers[i]->get_host_name() == "") {
+            listen_fd = open_listen_fd(NULL, port);
+        } else {
+            listen_fd = open_listen_fd(host_name, port);
+        }
+        std::cout << "[INFO] main: listen fd is " << listen_fd << std::endl;
+        if (listen_fd < 0) {
+            return EXIT_FAILURE;
+        }
+
+        HttpServer server(listen_fd);
+        server.multiple_io_server_run(*(servers[i]));
+    }
 
     return EXIT_FAILURE;
 }
